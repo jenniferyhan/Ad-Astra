b26ae0d9da866f7b369a36e04ac1863fc21a7ff4
darkmooninja <darkmooninja3481@gmail.com>
2024-02-09 21:28:57 -0800

    Update FlatSat_student.py

--- cubesat/FlatSat_student.py
+++ cubesat/FlatSat_student.py
@@ -40,19 +40,19 @@ def git_push():
     """
     This function is complete. Stages, commits, and pushes new images to your GitHub repo.
     """
-    try:
-        repo = Repo(REPO_PATH)
-        origin = repo.remote('origin')
-        print('added remote')
-        origin.pull()
-        print('pulled changes')
-        repo.git.add(REPO_PATH + FOLDER_PATH)
-        repo.index.commit('New Photo')
-        print('made the commit')
-        origin.push()
-        print('pushed changes')
-    except:
-        print('Couldn\'t upload to git')
+    # try:
+    repo = Repo(REPO_PATH)
+    origin = repo.remote('origin')
+    print('added remote')
+    origin.pull()
+    print('pulled changes')
+    repo.git.add(REPO_PATH + FOLDER_PATH)
+    repo.index.commit('New Photo')
+    print('made the commit')
+    origin.push()
+    print('pushed changes')
+    # except:
+        # print('Couldn\'t upload to git')
 
 
 def img_gen(name):
