a1c16583781939449d5924468dbe8137bf1cb6aa
darkmooninja <darkmooninja3481@gmail.com>
2024-02-09 20:02:19 -0800

    Update FlatSat_student.py

--- cubesat/FlatSat_student.py
+++ cubesat/FlatSat_student.py
@@ -20,7 +20,7 @@ import board
 from adafruit_lsm6ds.lsm6dsox import LSM6DSOX as LSM6DS
 from adafruit_lis3mdl import LIS3MDL
 from git import Repo
-from picamera2 import Picamera2
+from picamera2 import Picamera2, Preview
 
 #VARIABLES
 THRESHOLD = 20      #Any desired value from the accelerometer
